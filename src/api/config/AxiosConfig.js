87ad5b900621ca02c245d73b4381740a91e8e5a8
baistan <baistan.xi@gmail.com>
2022-11-19 05:08:01 +1300

    asd

---
+++ src/api/config/AxiosConfig.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+    baseURL: "http://localhost:8080",
+})
+
+axiosInstance.interceptors.request.use((config) => {
+    config.headers["Authorization"] = localStorage.getItem("token");
+    return config;
+})
+
+export default axiosInstance;
